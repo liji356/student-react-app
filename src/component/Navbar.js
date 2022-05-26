2229112c9b3c75ad1516569c93956ab7cc109868
liji356 <lijibthomas95@gmail.com>
2022-05-26 11:14:34 +0530

    new web

---
+++ src/component/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>
+<nav className="navbar navbar-expand-lg navbar-dark bg-success">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Student</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav">
+          <Link className="nav-link active" to={"/"}>Add Stud</Link>
+          <Link className="nav-link active" to={"/view"}>view Stud</Link>
+        
+      </div>
+    </div>
+  </div>
+</nav>
+
+
+
+    </div>
+  )
+}
+
+export default Navbar
